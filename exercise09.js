660be63bef27b40d91b01294dfd9706e2d34a04d
Igor Queirantes <igorqueirantes0@gmail.com>
2024-11-15 17:05:42 -0300

    Novos exercícios

---
+++ exercise09.js
@@ -0,0 +1,6 @@
+let velocity = 78.2;
+console.log(`A velocidade do seu carro é ${velocity}Km/h`);
+if (velocity > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança`);
---
+++ exercise10.js
@@ -0,0 +1,7 @@
+let country = 'Brasil';
+console.log(`Vivendo em ${country}`);
+if (country == 'Brasil') {
+    console.log('Você é Brasileiro!');
+} else {
+    console.log('Você é estrangeiro!')
+}
