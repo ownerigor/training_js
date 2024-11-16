10e58593058e8496138e0658e2f381ba29ac0dc5
Igor Queirantes <igorqueirantes0@gmail.com>
2024-11-16 00:05:57 -0300

    Projeto para detecção de horário atual

---
+++ project01/script.js
@@ -0,0 +1,19 @@
+function load() {
+    let message = document.querySelector('div#message');
+    let image = document.querySelector('img#image');
+    let date = new Date();
+    let hour = date.getHours();
+    
+    message.innerHTML = `Agora são ${hour} horas.`;
+    if (hour >= 0 && hour < 12) {
+        image.src = './images/morning_photo.jpg';
+        document.body.style.background = '#e2cd9f'
+    } else if (hour >= 12 && hour <= 18) {
+        image.src = './images/afternoon_photo.jpg';
+        document.body.style.background = '#b9846f'
+    } else {
+        image.src = './images/night_photo.jpg';
+        document.body.style.background = '#515154'
+    }
+}
+
